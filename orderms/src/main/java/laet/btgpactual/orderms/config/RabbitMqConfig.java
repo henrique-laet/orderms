4d0378b20879c1468b3eb41b7438ce7b39d5f2ba
henrique <henriquebaslaet@hotmail.com>
2025-01-31 16:32:45 -0300

    Configuring queue creation in RabbitMQ

---
+++ orderms/src/main/java/laet/btgpactual/orderms/config/RabbitMqConfig.java
@@ -0,0 +1,9 @@
+package laet.btgpactual.orderms.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    public static final String ORDER_CREATED_QUEUE = "btg-pactual-order-created";
+}
